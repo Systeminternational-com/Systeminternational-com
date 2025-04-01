b6169f8480bf9c3a28753e5e9f66d21d04a384ab
Futurefix <sathvikbimagani.9@gmail.com>
2025-04-01 11:48:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 document.querySelector('.menu-toggle').addEventListener('click', () => {
     document.querySelector('.sidebar').classList.toggle('active');
 });
+
+function signup() {
+    const email = document.getElementById('emailInput').value;
+    if (email) {
+        alert(`Thank you for signing up, ${email}!`);
+    } else {
+        alert('Please enter a valid email.');
+    }
+}
